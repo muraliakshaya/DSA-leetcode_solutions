45f1d72a15e3d751ae85ff943d21e3fba22c9f33
Akshaya Murali <37058426+muraliakshaya@users.noreply.github.com>
2025-02-01 00:00:45 +0530

    Time: 15 ms (36.6%), Space: 12.4 MB (39.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        og_list= str(x)
-        reverse= og_list[::-1]
-        return og_list==reverse
+        # og_list= str(x)
+        # reverse= og_list[::-1]
+        # return og_list==reverse
+
+        if x < 0:
+            return False
+        n = x    
+        rev = 0       
+        while x > 0:
+            rev = (rev * 10) + (x % 10)
+            x //= 10
+        
+        return rev == n
 
     
         
